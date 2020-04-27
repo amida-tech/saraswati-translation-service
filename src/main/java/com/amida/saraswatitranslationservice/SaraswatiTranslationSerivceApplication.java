9a1adcd34c930a621cb37b8fe71522399907c4af
Eric West <eawest2@gmail.com>
2020-04-27 16:23:37 -0400

    created base branch

---
+++ src/main/java/com/amida/saraswatitranslationservice/SaraswatiTranslationSerivceApplication.java
@@ -0,0 +1,13 @@
+package com.amida.saraswatitranslationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaraswatiTranslationSerivceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SaraswatiTranslationSerivceApplication.class, args);
+	}
+
+}

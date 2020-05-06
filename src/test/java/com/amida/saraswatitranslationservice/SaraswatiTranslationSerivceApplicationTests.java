8476058ad120f2f51b39f50eadf61d4ef7d31b60
Eric West <eawest2@gmail.com>
2020-05-06 08:31:42 -0400

    added new dependencies and test objects

--- src/test/java/com/amida/saraswatitranslationservice/SaraswatiTranslationSerivceApplicationTests.java
+++ src/test/java/com/amida/saraswatitranslationservice/SaraswatiTranslationSerivceApplicationTests.java
@@ -8,6 +8,12 @@ class SaraswatiTranslationSerivceApplicationTests {
 
 	@Test
 	void contextLoads() {
+		
+		String fmlTestString = "fmltestobjects/r3patient.json -output fmltestobjects/test.xml -transform fmltestobjects/PatientR3toR4.txt -ig ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
+		String fmlTestSource = "fmltestobjects/r3patient.json";
+		String fmlOutPutLocation = "fmltestobjects/test.xml";
+		String fmlMapLocation= "fmltestobjects/PatientR3toR4.txt";
+		String fmlInfoSource= "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
 	}
 
 }

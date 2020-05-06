4bb5fa3b56456e503fdb8ac005425fe51897cb0d
Eric West <eawest2@gmail.com>
2020-05-06 09:07:06 -0400

    added more ingests

--- src/test/java/com/amida/saraswatitranslationservice/SaraswatiTranslationSerivceApplicationTests.java
+++ src/test/java/com/amida/saraswatitranslationservice/SaraswatiTranslationSerivceApplicationTests.java
@@ -1,5 +1,12 @@
 package com.amida.saraswatitranslationservice;
 
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.net.URL;
+import java.nio.charset.StandardCharsets;
+
+import org.apache.commons.io.IOUtils;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,12 +15,40 @@ class SaraswatiTranslationSerivceApplicationTests {
 
 	@Test
 	void contextLoads() {
-		
-		String fmlTestString = "fmltestobjects/r3patient.json -output fmltestobjects/test.xml -transform fmltestobjects/PatientR3toR4.txt -ig ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
-		String fmlTestSource = "fmltestobjects/r3patient.json";
-		String fmlOutPutLocation = "fmltestobjects/test.xml";
-		String fmlMapLocation= "fmltestobjects/PatientR3toR4.txt";
-		String fmlInfoSource= "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
-	}
 
+		String fmlTestString = "fmltestobjects/r3patient.json -output fmltestobjects/test.xml -transform fmltestobjects/PatientR3toR4.txt -ig ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
+		String fmlTestSourceLocation = "fmltestobjects/r3patient.json";
+		String fmlOutPutLocation = "fmltestobjects/test.xml";
+		String fmlMapLocation = "fmltestobjects/PatientR3toR4.txt";
+		String fmlInfoSource = "ca.uhn.hapi.fhir:hapi-fhir-validation-resources-r4:3.8.0";
+
+		ClassLoader classLoader = getClass().getClassLoader();
+
+		URL resource1 = classLoader.getResource(fmlTestSourceLocation);
+		File fmlTestSourceData = new File(resource1.getFile());
+		try (InputStream inputStream = classLoader.getResourceAsStream(fmlTestSourceLocation)) {
+
+			String fmlTestSourceStream = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		URL resource2 = classLoader.getResource(fmlOutPutLocation);
+		File fmlOutPutLocationData = new File(resource2.getFile());
+		try (InputStream inputStream = classLoader.getResourceAsStream("xml/data.xml")) {
+
+			String fmlOutPutLocationStream = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+
+		URL resource3 = classLoader.getResource(fmlMapLocation);
+		File fmlMapLocationData = new File(resource3.getFile());
+		try (InputStream inputStream = classLoader.getResourceAsStream("xml/data.xml")) {
+
+			String fmlMapLocationStream = IOUtils.toString(inputStream, StandardCharsets.UTF_8);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
 }

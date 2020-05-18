ac1d014096687b9f9decf1f20f5ecb06b0b48575
Eric West <eawest2@gmail.com>
2020-05-18 13:26:45 -0400

    progressive update. A lot closer.

--- src/main/java/com/amida/saraswatitranslationservice/controller/PatientController.java
+++ src/main/java/com/amida/saraswatitranslationservice/controller/PatientController.java
@@ -1,9 +1,19 @@
 package com.amida.saraswatitranslationservice.controller;
 
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.ByteArrayInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.FileOutputStream;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
 
 import org.hl7.fhir.dstu3.model.Patient;
+import org.hl7.fhir.r5.context.SimpleWorkerContext;
+import org.hl7.fhir.r5.elementmodel.Element;
+import org.hl7.fhir.r5.elementmodel.Manager;
+import org.hl7.fhir.r5.elementmodel.Manager.FhirFormat;
+import org.hl7.fhir.r5.formats.IParser.OutputStyle;
 import org.hl7.fhir.validation.ValidationEngine;
 import org.hl7.fhir.validation.cli.model.CliContext;
 import org.hl7.fhir.validation.cli.services.ValidationService;
@@ -18,7 +28,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.amida.saraswatitranslationservice.utils.ApiResponse;
-import com.fasterxml.jackson.databind.ObjectMapper;
 
 @Controller
 @RequestMapping(path = "/api")
@@ -41,39 +50,45 @@ public class PatientController {
 	private String outputVersion;
 
 	@GetMapping(path = "/patient")
-	public @ResponseBody ResponseEntity<?> patientResponse(@RequestBody Patient requestPatient) {
+	public @ResponseBody ResponseEntity<?> patientResponse(@RequestBody byte[] request) {
 		ApiResponse apiResponse = new ApiResponse();
+		ByteArrayOutputStream returnOutput = new ByteArrayOutputStream();
 
 		if (conversionStatus) {
 			// Build the CLI command here out of our variables
 			Params params = new Params();
-			String[] args = { "-version" + inputVersion, "-to-version" + outputVersion, "" };
-			FileWriter file;
+			String[] args = { " -version" + inputVersion, " -to-version" + outputVersion, "" };
+			Content content = new Content();
+			content.cntType = FhirFormat.JSON;
+			content.focus = request;
+
 			try {
-				file = new FileWriter("./temp.json");
-				ObjectMapper mapper = new ObjectMapper();
-				String jsonString = mapper.writeValueAsString(requestPatient);
-	            file.write(jsonString);
-				try {
-					CliContext cliContext = params.loadCliContext(args);
-					ValidationEngine validator = ValidationService.getValidator(cliContext, null);
-					validator.convert(cliContext.getSources().get(0), cliContext.getOutput());
-				} catch (Exception e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-			} catch (IOException e1) {
+				CliContext cliContext = params.loadCliContext(args);
+				SimpleWorkerContext context = new SimpleWorkerContext();
+				ValidationEngine validator = ValidationService.getValidator(cliContext, null);
+
+				// Need to set up context to equal something, which is different than CLI
+				// context. It contains all of the information about what operation is being
+				// performed, and what version is being written to. Gotta dig that out.
+
+				Date date = Calendar.getInstance().getTime();
+				DateFormat dateFormat = new SimpleDateFormat("yyyy-mm-dd hh:mm:ss");
+				String strDate = dateFormat.format(date);
+				String output = "./temp/" + strDate + ".json";
+
+				// output needs to be rectified into something returnable. Currently it's a
+				// string representing a filepath destination. Theoretically this could be used
+				// then turned into a file read as the return. Currently returning empty
+				// byteArrayStream.
+				Element e = Manager.parse(context, new ByteArrayInputStream(content.focus), content.cntType);
+				Manager.compose(context, e, new FileOutputStream(output),
+						(output.endsWith(".json") ? FhirFormat.JSON : FhirFormat.XML), OutputStyle.PRETTY, null);
+			} catch (Exception e) {
 				// TODO Auto-generated catch block
-				e1.printStackTrace();
+				e.printStackTrace();
 			}
 
-
-
-
-			// Execute mapper here and set it as this test patient
-			Patient conversion = new Patient();
-
-			return new ResponseEntity<>(conversion, HttpStatus.OK);
+			return new ResponseEntity<>(returnOutput, HttpStatus.OK);
 		} else if (transformStatus) {
 			apiResponse.setResponseText("This feature is not yet implemented");
 			return new ResponseEntity<>(apiResponse, HttpStatus.NOT_IMPLEMENTED);
@@ -81,5 +96,10 @@ public class PatientController {
 			apiResponse.setResponseText("Incorrect Server Configuration");
 			return new ResponseEntity<>(apiResponse, HttpStatus.I_AM_A_TEAPOT);
 		}
+	}
+
+	private class Content {
+		byte[] focus = null;
+		FhirFormat cntType = null;
 	}
 }
